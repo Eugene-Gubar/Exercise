b6f95e0a67722cdc365b241173a103e60bcdf846
Eugene <eugene.gubar@gmail.com>
2017-03-16 21:58:00 +0200

    Finish basic realize timer on JavaScript native

--- ls3/ls2/js/script.js
+++ ls3/ls2/js/script.js
@@ -12,10 +12,12 @@ window.onload = function () {
             interval;
 
         /* ---------------------------------- begin [button] -----------------------------------*/
-        var btnStart  = document.getElementById('timer_start'),
-            btnStop   = document.getElementById('timer_stop'),
-            btnReset  = document.getElementById('timer_reset');
+        var btnStart  = getID('timer_start'),
+            btnStop   = getID('timer_stop'),
+            btnReset  = getID('timer_reset');
         /* ----------------------------------- end [button] ------------------------------------*/
+
+
 
         /* ---------------------------------- begin [events] -----------------------------------*/
         btnStart.onclick = startHandler;
@@ -23,17 +25,54 @@ window.onload = function () {
         btnReset.onclick = resetHandler;
         /* ----------------------------------- end [events] ------------------------------------*/
 
+       
+
         /* ---------------------------------- begin [handler] ----------------------------------*/
+        
         function startHandler() {
-            
-            minutes = parseInt(minutes / 60, 10)
-            seconds = parseInt(seconds % 60, 10);
+            var elTimerID = getID('id_timer');
 
-            minutes = minutes < 10 ? "0" + minutes : minutes;
-            seconds = seconds < 10 ? "0" + seconds : seconds;
+            interval = setInterval(function () {
 
+                minutes = parseInt(count / 60, 10)
+                seconds = parseInt(count % 60, 10);
+
+                minutes = minutes < 10 ? "0" + minutes : minutes;
+                seconds = seconds < 10 ? "0" + seconds : seconds;
+
+                count++;
+
+                elTimerID.innerHTML = minutes + ":" + seconds;
+
+            }, 1000);
         }
+
+        function stopHandler() {
+            clearInterval(interval);
+        }
+
+        function resetHandler() {
+            count    = 0,
+            minutes  = 0,
+            seconds  = 0,
+            mseconds = 0;
+        }
+
         /* ----------------------------------- end [handler] -----------------------------------*/
+
+
+
+        /* ------------------------------ begin [utility function] ---------------------------- */
+
+        function getID(id) {
+            return document.getElementById(id);
+        }
+
+        function getClass(c) {
+            return document.getElementsByClassName(c);
+        }
+
+        /* ------------------------------- end [utility function] ----------------------------- */
 
     })();
 
